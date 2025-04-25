eae0b97653d4bfbbd996092d2f750aeaa6862406
James Zhangly <jameszhang2012@yahoo.co.uk>
2025-04-25 03:20:24 +0100

    new horizon forecasting with naive

--- Training/Naive/Naive_Model.py
+++ Training/Naive/Naive_Model.py
@@ -4,7 +4,7 @@ class NaiveModel:
     def __init__(self):
         self.last_value = None
 
-    def predict(self, series, n_steps=1):
+    def predict(self, lastValue, n_steps=1):
         """Predict the last observed value for the next n steps."""
-        self.last_value = series.iloc[-1]
+        self.last_value = lastValue
         return np.full(n_steps, self.last_value)
